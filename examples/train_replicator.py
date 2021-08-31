ab6f4d8bbf933d097b617f06636c2b74efbc2e0b
feng wenfeng <fengwenfeng@gmail.com>
2021-08-31 22:11:25 +0800

    add trainer

--- examples/train_replicator.py
+++ examples/train_replicator.py
@@ -43,9 +43,9 @@ def evaluate_perplexity(net, validate_dataloader):
     net.eval()
     metric = Accumulator(2)
     for inputs, targets, masks in validate_dataloader:
-        # inputs = inputs.to(device)
-        # targets = targets.to(device)
-        # masks = masks.to(device)
+        inputs = inputs.to(device)
+        targets = targets.to(device)
+        masks = masks.to(device)
         loss = replicator_gpt(inputs, targets, masks)
         metric.add(loss, 1)
     loss = metric[0] / metric[1]
@@ -60,15 +60,21 @@ accelerator = Accelerator()
 print(accelerator.device)
 
 # %% fake dataset training using replicator_gpt
-blocks_num = 2
+blocks_num = 4
 replicator_gpt = ReplicatorGPT(blocks_num=blocks_num, max_sentence_len=max_sentence_len,
                     vocab_size=vocab_size, embedding_size=embedding_size, mask=True).to(device)
 
-lr = 1e-3
+lr = 1e-2
 optimizer = torch.optim.Adam(replicator_gpt.parameters(), lr=lr)
 
 num_epochs = 5
 
+# %%
+from replicator.trainings.train_replicator import Trainer
+
+trainer = Trainer(replicator_gpt, wikitext2_train, optimizer)
+trainer.train(num_epochs)
+# %%
 replicator_gpt, optimizer, train_dataloader, validate_dataloader = accelerator.prepare(
     replicator_gpt, optimizer, train_dataloader, validate_dataloader)
 
@@ -108,8 +114,8 @@ replicator_gpt.train()
 for epoch in range(num_epochs):
     replicator_gpt.train()
     epoch_metric = Accumulator(2)
-    for inputs, targets in wikitext2_train:
-        masks = torch.ones_like(inputs).bool()
+    for inputs, targets, masks in wikitext2_train:
+        # masks = torch.ones_like(inputs).bool()
         inputs = inputs.to(device)
         targets = targets.to(device)
         masks = masks.to(device)
@@ -134,13 +140,13 @@ nlayers = 2  # number of nn.TransformerEncoderLayer in nn.TransformerEncoder
 nhead = 2  # number of heads in nn.MultiheadAttention
 dropout = 0.2  # dropout probability
 
-model = TransformerModel(vocab_size, embedding_size, nhead, d_hid, nlayers, dropout).to(device)
+model = TransformerModel(max_sentence_len, vocab_size, embedding_size, nhead, d_hid, nlayers, dropout).to(device)
 
 lr = 1e-3
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
-# model, optimizer, wikitext2_train, wikitext2_val = accelerator.prepare(
-#     model, optimizer, wikitext2_train, wikitext2_val)
+model, optimizer, wikitext2_train, wikitext2_val = accelerator.prepare(
+    model, optimizer, wikitext2_train, wikitext2_val)
 
 # %%
 
@@ -148,22 +154,22 @@ criterion = nn.CrossEntropyLoss()
 
 num_epochs = 5
 
-src_mask = generate_square_subsequent_mask(max_sentence_len)
+# src_mask = generate_square_subsequent_mask(max_sentence_len)
 
 model.train()
 for epoch in range(num_epochs):
     model.train()
     epoch_metric = Accumulator(2)
-    for inputs, targets in wikitext2_train:
-        inputs = inputs.to(device)
-        targets = targets.to(device)
+    for inputs, targets, masks in wikitext2_train:
+        inputs = inputs.to(accelerator.device)
+        targets = targets.to(accelerator.device)
         optimizer.zero_grad()
-        outputs = model(inputs, src_mask)
-        loss = criterion(outputs.view(-1, vocab_size), targets.reshape(-1))
+        loss = model(inputs, targets)
+        # loss = criterion(outputs.view(-1, vocab_size), targets.reshape(-1))
         print(loss)
         # with torch.autograd.set_detect_anomaly(True):
-        loss.backward()
-        # accelerator.backward(loss)
+        # loss.backward()
+        accelerator.backward(loss)
         optimizer.step()
         with torch.no_grad():
             epoch_metric.add(loss, 1)
