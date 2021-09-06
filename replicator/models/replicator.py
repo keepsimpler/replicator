28b9987dc595e6ee915f20be33b68545929af852
feng wenfeng <fengwenfeng@gmail.com>
2021-09-06 22:38:15 +0800

    wikitext103

--- replicator/models/replicator.py
+++ replicator/models/replicator.py
@@ -261,4 +261,5 @@ class ReplicatorGPT(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr = self.lr)
+        # scheduler = 
         return optimizer
